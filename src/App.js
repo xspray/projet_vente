ba8cec1e0e8a18b291e784df3e76bfc99e8a96c0
xspray <propagepress@hotmail.com>
2020-10-17 18:38:47 +0200

    Intégration Redux dans le projet, userReducer et userActions au Header et App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shopping/shopping.component";
 import LoginEtInscription from "./pages/login-et-inscription/login-et-inscription.component";
 import Header from "./components/header/header.component";
 import { auth, creerUserProfilDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   deconnexionDeAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.deconnexionDeAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await creerUserProfilDocument(userAuth);
 
         userRef.onSnapshot((capture) => {
-          this.setState(
-            {
-              currentUser: {
-                id: capture.id,
+          setCurrentUser({
+            currentUser: {
+              id: capture.id,
 
-                ...capture.data(),
-              },
-            }
-          );
+              ...capture.data(),
+            },
+          });
         });
       } else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -47,7 +40,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
 
         <Switch>
           <Route exact path="/" component={HomePage} />
@@ -61,4 +54,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
+import store from './redux/store'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById("root")
 );
